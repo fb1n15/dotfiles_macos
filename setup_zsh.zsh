0597cae81ed4d71fce164e2417cf71a67c17b109
Chandler <fb1n15@users.noreply.github.com>
2021-10-21 12:28:39 +0100

    Add setup_zsh.zsh and matching Dotbot command
    Add homebrew-installed zsh location to /etc/shells and change login shell.

---
+++ setup_zsh.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+echo "\n<<< Starting ZSH Setup >>>\n"
+
+# no need for installation; it's in the Brewfile.
+
+echo "Enter sudo password to edit /etc/shells"
+echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells'
+
+echo "Enter user password to chagne login shell"
+chsh -s '/usr/local/bin/zsh'
