fd1dc57d2c96c879b788580f82e826a9120d6d80
Chandler <fb1n15@users.noreply.github.com>
2021-10-21 10:20:59 +0100

    Add comments to the exist() function

---
+++ shebang.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Running $0 >>>\n"
+
+exists brew && echo "excellent!!!" || echo "bogus"
+exists nonexistent && echo "excellent" || echo "bogus!!!"
--- zshenv
+++ zshenv
@@ -1,5 +1,6 @@
 echo 'Hello from .zshenv'
 
 function exists() {
+  # `command -v` is similar to `which`
   command -v $1 >/dev/null 2>&1
 }
