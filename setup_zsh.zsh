6ef6846284c4989dfd77283392113831a09c5843
Chandler <fb1n15@users.noreply.github.com>
2021-10-21 15:26:45 +0100

    Symlink sh to zsh

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -1,10 +1,27 @@
 #!/usr/bin/env zsh
 echo "\n<<< Starting ZSH Setup >>>\n"
 
-# no need for installation; it's in the Brewfile.
+if grep -Fxq '/usr/local/bin/zsh' '/etc/shells'; then
+  echo '/usr/local/bin/zsh already exists in /etc/shells'
+else
+  echo "Enter sudo password to edit /etc/shells"
+  echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+fi
 
-echo "Enter sudo password to edit /etc/shells"
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
+  echo '$SHELL is already /usr/local/bin/zsh'
+else
+  echo "Enter user password to chagne login shell"
+  chsh -s '/usr/local/bin/zsh'
+fi
 
-echo "Enter user password to chagne login shell"
-chsh -s '/usr/local/bin/zsh'
+if sh --version | grep -q zsh; then
+  echo '/private/var/select/sh already linked to /bin/zsh'
+else
+  echo "E_nter superuser password to symlink sh to zsh"
+  # Looked cute, might delete later, idk
+  sudo ln -sfv /bin/zsh /private/var/select/sh 
+
+  # # I'd like for this to work instead (zsh installed by brew)
+  # sudo ln -sfv /usr/local/bin/zsh /private/var/select/sh 
+fi
--- shebang.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-echo "\n<<< Running $0 >>>\n"
-
-exists brew && echo "excellent!!!" || echo "bogus"
-exists nonexistent && echo "excellent" || echo "bogus!!!"
