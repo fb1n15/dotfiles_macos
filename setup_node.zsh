4d8ec193274c442b3643bd0e7dcdef830ace8f08
Chandler <fb1n15@users.noreply.github.com>
2021-10-22 15:23:46 +0100

    Add setup_node.zsh and matching Dotbot command

---
+++ setup_node.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+# shellcheck disable=SC2028
+echo "\n<<< Starting Node Setup >>>\n"
+
+# Node versions are managed with `n`, which is in the Brewfile.
+#See zshrc for N_PREFIX variable and addition to PATH.
+
+if exists node; then
+  echo "Node $(node --version) & NPM $(npm --version) already installed"
+else
+  echo "Installing Node & NPM with n..."
+  n latest
+fi
+
